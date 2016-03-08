7ca293a2a1af5d48530be90c29d19c96d70c7f39
sleepbear <sleepbear669@gmail.com>
2016-03-08 17:53:50 +0900

    fist commit

---
+++ src/main/java/com/github/webing/webingApp/WebingApplication.java
@@ -0,0 +1,14 @@
+package com.github.webing.webingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by sleepbear on 2016. 2. 11..
+ */
+@SpringBootApplication
+public class WebingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebingApplication.class, args);
+    }
+}
