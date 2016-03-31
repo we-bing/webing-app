4d92a674d48923156911f125395c5fb4462dd70c
kd4 <kangddanddan@gmail.com>
2016-03-31 18:43:30 +0900

    Update Maven configurations to deploy war to tomcat.

--- src/main/java/com/github/webing/webingApp/WebingApplication.java
+++ src/main/java/com/github/webing/webingApp/WebingApplication.java
@@ -2,12 +2,20 @@ package com.github.webing.webingApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 /**
  * Created by sleepbear on 2016. 2. 11..
  */
 @SpringBootApplication
-public class WebingApplication {
+public class WebingApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebingApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(WebingApplication.class, args);
     }
