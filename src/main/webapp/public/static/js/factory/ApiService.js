8b759ab01b83137e45304d98468cde48b7cb45ef
sleepbear <sleepbear669@gmail.com>
2016-04-05 18:29:31 +0900

    Add page icon

--- src/main/webapp/public/static/js/factory/ApiService.js
+++ src/main/webapp/public/static/js/factory/ApiService.js
@@ -3,33 +3,34 @@
  */
 
 webing.service("apiService", function($http) {
+    var apiUrl = "http://117.17.102.241:8080";
     return {
         districts: function () {
-            var url = '/api/townList';
+            var url = apiUrl + '/api/townList';
             return $http.get(url);
         },
         cityList: function () {
-            var url = '/api/cityList';
+            var url = apiUrl + '/api/cityList';
             return $http.get(url);
         },
         countyList: function (cityCode) {
-            var url = '/api/countyList/'+cityCode ;
+            var url = apiUrl + '/api/countyList/'+cityCode ;
             return $http.get(url);
         },
         townList: function (countyCode) {
-            var url = '/api/townList/'+countyCode;
+            var url = apiUrl + '/api/townList/'+countyCode;
             return $http.get(url);
         },
         candidacise: function(districtCode) {
-            var url = '/api/candidacies/' + districtCode;
+            var url = apiUrl + '/api/candidacies/' + districtCode;
             return $http.get(url);
         },
         assemblyMember: function(assemblyMemberId) {
-            var url = '/api/assemblyMember/' + assemblyMemberId;
+            var url = apiUrl + '/api/assemblyMember/' + assemblyMemberId;
             return $http.get(url);
         },
         news : function(candidacyName, newsKeyword) {
-            var url = '/api/newsKeywords';
+            var url = apiUrl + '/api/newsKeywords';
             var config = {
                 params: {
                     query: candidacyName + " " + newsKeyword
