7e3995b1c6e046d19e7d98a73a93f3d00a89fb8d
sleepbear <sleepbear669@gmail.com>
2016-04-07 19:59:33 +0900

    Remove filter

--- src/main/java/com/github/webing/webingApp/WebingApplication.java
+++ src/main/java/com/github/webing/webingApp/WebingApplication.java
@@ -2,13 +2,6 @@ package com.github.webing.webingApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.web.filter.CharacterEncodingFilter;
-
-import javax.servlet.Filter;
-import java.nio.charset.Charset;
 
 /**
  * Created by sleepbear on 2016. 2. 11..
@@ -21,18 +14,7 @@ public class WebingApplication {
 //    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 //        return application.sources(WebingApplication.class);
 //    }
-    @Bean
-    public HttpMessageConverter<String> responseBodyConverter() {
-        return new StringHttpMessageConverter(Charset.forName("UTF-8"));
-    }
 
-    @Bean
-    public Filter characterEncodingFilter() {
-        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
-        characterEncodingFilter.setEncoding("UTF-8");
-        characterEncodingFilter.setForceEncoding(true);
-        return characterEncodingFilter;
-    }
     public static void main(String[] args) {
         SpringApplication.run(WebingApplication.class, args);
     }
